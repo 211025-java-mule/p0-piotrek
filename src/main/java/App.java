6af7a55b5688f6eaee22e782517f0922fe4a70d8
Piotr Boczar <boczarpiotr@gmail.com>
2021-10-28 11:28:50 +0200

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        String choice = args[0];
+
+        URL url = new URL("https://rickandmortyapi.com/api/character/" + choice);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Accept", "application/json");
+        InputStream response = connection.getInputStream();
+        String body = new String(response.readAllBytes());
+        System.out.println(body);
+
+    }
+}
